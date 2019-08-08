6e6bff10bdae936c3894847d538f0b683c3537b8
RiqCodes <keekman3@gmail.com>
2019-08-08 02:12:31 -0700

    semi working map commented chart component

--- client/src/containers/Map/Fmap.js
+++ client/src/containers/Map/Fmap.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import img from '../Map/FORTNITESEASON10MAP.jpg'
 import axios from "axios";
-var CanvasJSReact = require('./canvasjs.react');
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+// var CanvasJSReact = require('./canvasjs.react');
+// var CanvasJS = CanvasJSReact.CanvasJS;
+// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 
 class Fmap extends Component {
@@ -19,13 +19,13 @@ class Fmap extends Component {
 
         //POST API CALL TO BACKEND which sends a Coord Object {x float, y float, weight int}
         // and recived an updated coordsArray
-        // try {
-        //     const response = await axios.post('http://localhost:3001/api/SendVote', { clickCoords: '' });
-        //     console.log('Returned data:', response);
-        // } catch (e) {
-        //     this.setState({ currentVote: clickCoords })
-        //     console.log(`Axios request failed: ${e}`);
-        // }
+        try {
+            const response = await axios.post('http://localhost:3001/api/SendVote', { clickCoords: '' });
+            console.log('Returned data:', response);
+        } catch (e) {
+            this.setState({ currentVote: clickCoords })
+            console.log(`Axios request failed: ${e}`);
+        }
 
     }
 
@@ -41,55 +41,55 @@ class Fmap extends Component {
         );
     }
 
-//      <br/> //Just so that JSFiddle's Result label doesn't overlap the Chart
-// <div id="chartContainer" style="height: 100%; width: 100%;
-//  background-image:url('https://www.mapsofindia.com/worldmap/map-of-world.jpg');">
-//  </div>
+// //      <br/> //Just so that JSFiddle's Result label doesn't overlap the Chart
+// // <div id="chartContainer" style="height: 100%; width: 100%;
+// //  background-image:url('https://www.mapsofindia.com/worldmap/map-of-world.jpg');">
+// //  </div>
 
-    render() {
+//     render() {
        
-		const options = {
-            backgroundColor: "transparent",
-			animationEnabled: true,
-			exportEnabled: true,
-			theme: "", // "light1", "light2", "dark1", "dark2"
-			title:{
-				text: "Drop here!",
-			fontSize: 26
-			},
-			axisX: {
-				title: "x",
-			logarithmic: true
-			},
-			axisY: {
-				title: "y"
-			},
-			data: [{
-				type: "bubble",
-				indexLabel: "{label}",
-				toolTipContent: "<b>{label}</b><br>:{x}<br>:{y}<br>:{z}",
-				dataPoints: [
-					{ label: "Mercury", x: 36, y: 452, z: 3031 },
-					{ label: "Venus", x: 67, y: 726, z: 7521 },
-					{ label: "Earth", x: 93, y: 285, z: 7926 },
-					{ label: "Mars", x: 141, y: 230, z: 4222 },
-					{ label: "Jupiter", x: 483, y: 120, z: 88729 },
-					{ label: "Saturn", x: 886, y: 88, z: 74600 },
-					{ label: "Uranus", x: 1784, y: 59, z: 32600 },
-					{ label: "Neptune", x: 2794, y: 48, z: 30200 },
-				]
-            }]
+// 		const options = {
+//             backgroundColor: "transparent",
+// 			animationEnabled: true,
+// 			exportEnabled: true,
+// 			theme: "", // "light1", "light2", "dark1", "dark2"
+// 			title:{
+// 				text: "Drop here!",
+// 			fontSize: 26
+// 			},
+// 			axisX: {
+// 				title: "x",
+// 			logarithmic: true
+// 			},
+// 			axisY: {
+// 				title: "y"
+// 			},
+// 			data: [{
+// 				type: "bubble",
+// 				indexLabel: "{label}",
+// 				toolTipContent: "<b>{label}</b><br>:{x}<br>:{y}<br>:{z}",
+// 				dataPoints: [
+// 					{ label: "Mercury", x: 36, y: 452, z: 3031 },
+// 					{ label: "Venus", x: 67, y: 726, z: 7521 },
+// 					{ label: "Earth", x: 93, y: 285, z: 7926 },
+// 					{ label: "Mars", x: 141, y: 230, z: 4222 },
+// 					{ label: "Jupiter", x: 483, y: 120, z: 88729 },
+// 					{ label: "Saturn", x: 886, y: 88, z: 74600 },
+// 					{ label: "Uranus", x: 1784, y: 59, z: 32600 },
+// 					{ label: "Neptune", x: 2794, y: 48, z: 30200 },
+// 				]
+//             }]
            
-		}
-		return (
-		<div id="chartcontainer" style="height: 1000px; width: 100%; background-image:url('https://files.slack.com/files-pri/TH7BS1571-FLZLVBW64/fortniteseason10map.jpg');">
-			<CanvasJSChart options = {options}
-				/* onRef={ref => this.chart = ref} */
-			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		</div>
-		);
-	}
+// 		}
+// 		return (
+// 		<div id="chartcontainer" style={{height: "1000px", width: "100%", backgroundImage: ('https://files.slack.com/files-pri/TH7BS1571-FLZLVBW64/fortniteseason10map.jpg')}}>
+// 			{/* <CanvasJSChart options = {options} */}
+// 				/* onRef={ref => this.chart = ref} */
+// 			/>
+// 			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+// 		</div>
+// 		);
+// 	}
 
  
 }
