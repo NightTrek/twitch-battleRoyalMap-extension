07e7ebb799d75f5b3a223c7c84c10e36f7dd2016
Daniel <Daniels@dual4t.com>
2020-03-27 19:43:26 -0700

    pointed to the index.html in the build folder

--- index.js
+++ index.js
@@ -88,7 +88,9 @@ if(process.env.NODE_ENV === 'production') {
 
     app.get('/*', (req, res) => {
         // res.sendFile(path.join(__dirname, '../build/'))
-        res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+        console.log("servering the catchall get req");
+
+        res.sendFile(path.resolve( 'client','build', 'index.html'));
     })
 }
 // else{
